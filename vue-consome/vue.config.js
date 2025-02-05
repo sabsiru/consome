412441749adec65d2e8ca9129ba9bbde73ba3144
sabsiru <zero013@naver.com>
2025-02-05 14:36:24 +0900

    웹소켓 에러 수정

--- vue-consome/vue.config.js
+++ vue-consome/vue.config.js
@@ -1,15 +1,21 @@
-// const { defineConfig } = require("@vue/cli-service");
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-// });
-module.exports = {
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
   devServer: {
+    client: {
+      logging: "info",
+      overlay: true,
+      progress: true,
+      reconnect:3,
+    },
     proxy: {
-      "/api": {
+      "/": {
         target: "http://localhost:8080",
         changeOrigin: true,
-        pathRewrite: { "^/api": "" }
+        logLevel: "debug",
+        ws: true,
+        pathRewrite: { "^/": "" }
       }
     }
   },
-};
+});
